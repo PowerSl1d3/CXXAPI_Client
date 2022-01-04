966a786f64b6867c5119b24f572b86c8bce5e7e2
PowerSl1d3 <manikryte@gmail.com>
2022-01-03 23:50:18 -0800

    [#1] Fixed raw path to saving files

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from client import Client
+import sys
 import magic
 import time
 
@@ -8,14 +9,16 @@ if __name__ == '__main__':
     username = 'admin'
     password = 'admin'
     client = Client(url=url, username=username, password=password)
+    if len(sys.argv):
+        current_path = sys.argv[0]
+    print(client.post_file(f'{current_path}/HelloWorld.cpp',
+                           magic.from_file(f'{current_path}/HelloWorld.cpp')))
     #print(client.get_todolist())
     #print(client.post_todo("Hello world!"))
-    #print(client.update_todo(1, "New todo"))
+    #print(client.update_todo(1, "New todo")
     #print(client.delete_todo(1))
-    print(client.post_file('/home/olegaksenenko/Desktop/git/CXXAPI_Client/HelloWorld.cpp',
-                           magic.from_file('/home/olegaksenenko/Desktop/git/CXXAPI_Client/HelloWorld.cpp')))
-    file_list = client.get_file_list()
-    print(file_list)
-    print(client.get_file(file_list[0][1], file_list[0][2]))
-    print(client.delete_file(file_list[0][1], file_list[0][2]))
+    #file_list = client.get_file_list()
+    #print(file_list)
+    #print(client.get_file(file_list[0][1], file_list[0][2]))
+    #print(client.delete_file(file_list[0][1], file_list[0][2]))
 
